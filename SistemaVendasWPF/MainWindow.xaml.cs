beff2e811410f7384e45706af8b3ff1f0938a1a2
BrunoMaia <brunomaiadesenv@gmail.com>
2026-03-23 21:23:46 -0300

    torna os campos de input de dados dinamicos na interface WPF

--- SistemaVendasWPF/MainWindow.xaml.cs
+++ SistemaVendasWPF/MainWindow.xaml.cs
@@ -22,19 +22,38 @@ public partial class MainWindow : Window
 
         try
         {
-            // Dados simulados da UI
-            int produtoId = 101;
-            int qtde = 1;
-            decimal valorTotal = 1500.00m;
+            // VALIDAÇÃO E CAPTURA DOS DADOS DIGITADOS NA TELA
+            
+            // Tenta converter o que foi digitado no campo de valor para número
+            if (!decimal.TryParse(txtValor.Text, out decimal valorTotal))
+            {
+                txtStatus.Text = "Erro: Digite um valor válido.";
+                txtStatus.Foreground = Brushes.Red;
+                return;
+            }
+
             string cartao = txtCartao.Text;
             string email = txtEmail.Text;
 
-            // CHAMA A FACHADA (Padrão GoF em ação)
+            
+            if (string.IsNullOrWhiteSpace(cartao) || string.IsNullOrWhiteSpace(email))
+            {
+                txtStatus.Text = "Erro: Preencha o cartão e o e-mail.";
+                txtStatus.Foreground = Brushes.Red;
+                return;
+            }
+            
+            int produtoId = 101; 
+            int qtde = 1;
+
+            // CHAMA A FACHADA 
             string resultado = _vendaFacade.FinalizarPedido(produtoId, qtde, valorTotal, cartao, email);
 
+            // MOSTRA O RESULTADO NA TELA
             txtStatus.Text = resultado;
             txtStatus.Foreground = resultado.Contains("Sucesso") ? Brushes.Green : Brushes.Red;
             
+            // desabilita o botão para não clicar duas vezes
             if (resultado.Contains("Sucesso")) btnFinalizar.IsEnabled = false;
         }
         catch (Exception ex)
